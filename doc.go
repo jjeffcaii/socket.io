d277e0ed991f63caa9f11a6991fdb463a46d1db9
jjeffcaii <jjeffcaii@outlook.com>
2017-11-02 14:29:34 +0800

    这个人很懒, 往代码库丢了一只战斗力13775的皮卡丘.

---
+++ doc.go
@@ -0,0 +1,20 @@
+// Package sio is a server-side Socket.IO in Golang.
+// Basic Example - Echo Server: recieve messages and echo them back.
+//
+// flag.Parse()
+//
+// server := sio.NewBuilder().Build()
+//
+// nsp := server.Of("/")
+//
+// nsp.OnConnect(func(socket sio.Socket) {
+//   socket.On("news", func(msg sio.Message) {
+//     fmt.Println("[news]:", msg)
+//   })
+// 	 socket.Emit("hello", "你好，世界！")
+// })
+//
+// http.HandleFunc(sio.DefaultPath, server.Router())
+// log.Fatalln(http.ListenAndServe(":3000", nil))
+//
+package sio
